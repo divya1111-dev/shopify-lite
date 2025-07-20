bb128648072bb6e7e0f41f1074e53f6370cca088
divya1111-dev <divyamithileshsingh@gmail.com>
2025-07-20 20:34:03 +0530

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'category',
+        'price',
+        'description',
+        'details',
+        'image',
+        'data_ai_hint',
+    ];
+}
+
+class CartItem
+{
+    public Product $product;
+    public int $quantity;
+
+    public function __construct(Product $product, int $quantity)
+    {
+        $this->product = $product;
+        $this->quantity = $quantity;
+    }
+}
